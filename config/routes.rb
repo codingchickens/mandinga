2012d297dc9198400ae90a7e3d3862d3dc46722f
Melissa Vildosola <melissa@codingchickens.com>
2024-06-06 17:35:29 -0400

    feat: adds index warehouse_form

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get "salida" => "landing#salida"
 
   post "/get_form" => "warehouse_form#get_form"
+  get "formularios" => "warehouse_form#index"
+  #get "form_entry" => "warehouse_form#form_entry"
+  get "form_out" => "warehouse_form#form_out"
 end
